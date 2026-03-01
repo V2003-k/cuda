e2d82a59d4b7a94af7ec753dd8a7ee625461c5a7
V2003-k <vishwajeetkhaple@gmail.com>
2026-03-01 23:33:40 +0530

    macros and global

---
+++ C and C++ Review/04_macros_and_global/01.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+// examples for each conditional macro
+// #if
+// #ifdef
+// #ifndef
+// #elif
+// #else
+// #endif
+
+#define PI 3.14159
+#define AREA(r) (PI * r * r)
+
+#ifndef radius
+#define radius 7
+#endif
+
+// if elif else logic
+// we can only use integer constants in #if and #elif
+#if radius > 10
+#define radius 10
+#elif radius < 5
+#define radius 5
+#else
+#define radius 7
+#endif
+
+int main(){
+    printf("Area of circle with radius %.2d: %f\n", radius, AREA(radius));
+}
